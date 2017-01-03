9a2e7e2a9939b401e0ea7f448ce9a6a5f2a487c9
Yelena Gindin <yelenar@gmail.com>
2017-01-03 10:29:53 +0200

    CLI

---
+++ cli/c.js
@@ -0,0 +1,76 @@
+var GameField = require('../src/GameField.js');
+var readline = require('readline');
+
+
+var gameField = new GameField.GameField();
+
+function printField(field)
+{
+    for (var y = 0; y < 3; y++) {
+        for (var x = 0; x < 3; x++) {
+            switch (field.get(y, x)) {
+            case GameField.Values.EMPTY:
+                process.stdout.write('.');
+                break;
+            case GameField.Values.X:
+                process.stdout.write('X');
+                break;
+            case GameField.Values.O:
+                process.stdout.write('O');
+                break;
+            default:
+                process.stdout.write('?');
+                break;
+            }
+        }
+        process.stdout.write("\n");
+    }
+}
+
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+printField(gameField);
+rl.setPrompt("ttt> ");
+rl.prompt();
+
+
+
+rl.on('line', function(line) {
+    var splitInput = line.split(" ");
+    if (splitInput.length != 3) {
+        console.log("Unexpected argument");
+        printField(gameField);
+        rl.prompt();
+        return;
+    }
+
+    if (isNaN(splitInput[1]) || isNaN(splitInput[2])) {
+        console.log("Invalid integer");
+        printField(gameField);
+        rl.prompt();
+        return;
+    }
+
+    var x = parseInt(splitInput[1]);
+    var y = parseInt(splitInput[2]);
+
+
+    switch (splitInput[0]) {
+    case 'X':
+        gameField.set(GameField.Values.X, x, y);
+        break;
+    case 'O':
+        gameField.set(GameField.Values.O, x, y);
+        break;
+    default:
+        console.log("Unknown command");
+        break;
+    }
+    printField(gameField);
+    rl.prompt();
+});
