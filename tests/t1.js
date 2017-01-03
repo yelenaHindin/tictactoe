f32da57736ebd6362a08dd65f695c706b7e2f806
Yelena Gindin <yelenar@gmail.com>
2017-01-03 10:18:30 +0200

    Fixing assertions, values, args

--- tests/t1.js
+++ tests/t1.js
@@ -3,14 +3,16 @@ var should = require('should');
 
 describe('Sanity', function() {
     it('Initialization', function() {
-        var gameField = new Gamefield.Gamefield();
+        var gameField = new GameField.GameField();
     });
 
     it('Set/get', function() {
-        var gameField = new Gamefield.Gamefield();
-        gameField.get(0, 0).should.be.equal(GameField.Values.EMPTY);
+        var gameField = new GameField.GameField();
+        var v = gameField.get(0, 0);
+        (v !== undefined).should.be.true();
+        v.should.be.equal(GameField.Values.EMPTY);
 
         gameField.set(GameField.Values.X, 0, 0);
-        gameField.get(GameField.Values.X, 0, 0).should.be.equal(GameField.Values.X);
+        gameField.get(0, 0).should.be.equal(GameField.Values.X);
     });
 });
