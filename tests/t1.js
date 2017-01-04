229531222a555265adecb6674091b844f18a2438
Yelena Gindin <yelenar@gmail.com>
2017-01-04 08:54:07 +0200

    Listeners unit test

--- tests/t1.js
+++ tests/t1.js
@@ -2,17 +2,35 @@ var GameField = require('../src/GameField.js');
 var should = require('should');
 
 describe('Sanity', function() {
-    it('Initialization', function() {
-        var gameField = new GameField.GameField();
+    var gameField;
+
+    beforeEach('Set gameField', function() {
+        gameField = new GameField.GameField();
     });
 
+
     it('Set/get', function() {
-        var gameField = new GameField.GameField();
         var v = gameField.get(0, 0);
         (v !== undefined).should.be.true();
         v.should.be.equal(GameField.Values.EMPTY);
 
         gameField.set(GameField.Values.X, 0, 0);
         gameField.get(0, 0).should.be.equal(GameField.Values.X);
+    });
+
+    it('Listener', function() {
+        var callbackState;
+
+        function callback(v, x, y) {
+            callbackState = {v: v, x: x, y: y };
+        }
+
+        gameField.addListener(callback, this);
+
+        gameField.set(GameField.Values.X, 1, 2);
+
+        callbackState.v.should.be.equal(GameField.Values.X);
+        callbackState.x.should.be.equal(1);
+        callbackState.y.should.be.equal(2);
     });
 });
